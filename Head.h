4a6c9ef6bfc0d824bad3b24f1d913bc98b359832
Amr Mohamed <AmrMohamed222>
2022-09-03 21:27:38 +0200

    1.0.0

---
+++ Head.h
@@ -0,0 +1,6 @@
+#ifndef HEAD_H_INCLUDED
+#define HEAD_H_INCLUDED
+
+int sum_values(int a,int b);
+
+#endif // HEAD_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Head.h"
+int main()
+{
+    int x = 10;
+    int y = 20;
+    printf("Sum of %d & %d = %d\n",x,y,sum_values(x,y));
+
+    return 0;
+}
